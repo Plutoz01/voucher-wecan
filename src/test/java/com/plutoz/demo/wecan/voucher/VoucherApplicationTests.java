6a6f6646d0af50a170f0e671a19734e4cb607f30
László Csatlós <plutoz01@gmail.com>
2023-02-24 10:51:53 +0100

    Init project

---
+++ src/test/java/com/plutoz/demo/wecan/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plutoz.demo.wecan.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
